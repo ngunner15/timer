a98cb263547d72ffe9c1e329a761687d0d8523ec
Nisarg <nisargvv@gmail.com>
2020-12-01 18:53:03 +0000

    Added validation

--- timer1.js
+++ timer1.js
@@ -2,9 +2,11 @@ const args = process.argv.slice(2);
 
 const timer = function(time) {
   for (let i = 0; i < time.length; i++) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, args[i] * 1000);
+    if (Number(args[i]) / 1 !== NaN && args[i] >= 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, args[i] * 1000);
+    }
   }
 };
 
